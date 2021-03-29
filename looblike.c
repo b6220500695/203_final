aef7d9f9547d97d5d531a220f2cabc8e83c1f721
mmint.m <pheeraya.p@ku.th>
2021-03-29 17:29:52 +0700

    1

--- looblike.c
+++ looblike.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 int main()
 {
-   int N,n,max ;
+   int N,n,max,x[1000],i ;
    printf("Input\n");
    scanf("%d",&n);
+   for(i=0;i<=10001;i++)
+   {
+      x[i]=0;
+   }
+   for(i=0;i<n;i++)
+   {
 
+   }
 
 
 }
