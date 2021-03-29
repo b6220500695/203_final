c3cc666684009efc3518d8b7719e4e1eca16307b
mmint.m <pheeraya.p@ku.th>
2021-03-29 19:30:36 +0700

    1

--- Nearest Number.c
+++ Nearest Number.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 int main()
 {
-   int k,c,a,n;
+   int k,c,n,i;
    scanf("%d %d",&n,&c);
    if((n>0)&&(n<32750))
    {
       {
       n=n/10;
       k=n;
-      a=(k/10)+(k%10);
-       printf("%d",a);
+      printf("%d",k%10);
       }
-      if(k>c)
-      k=n/10;
-      printf(" %d ",k+1);
+      k=k*100;
+if(k>n)
+      {
+      n=n/10;
+      k=n;
+      printf("%d",k%10+1);
+      }
+
       printf("%d",c);
-   }
+      }
    return 0;
 }
