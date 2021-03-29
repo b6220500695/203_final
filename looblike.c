915b7ba9af5465a22b02cad084a190271b51bc74
mmint.m <pheeraya.p@ku.th>
2021-03-29 18:00:44 +0700

    1

--- looblike.c
+++ looblike.c
@@ -1,32 +1,18 @@
 #include<stdio.h>
 int main()
 {
-   int N,n,max,x[1000],i ;
-   printf("Input\n");
-   scanf("%d",&n);
+   int N,n,max,i ;
+   int x[1001];
+   scanf("%d",&N);
    for(i=0;i<=10001;i++)
    {
       x[i]=0;
    }
-   for(i=0;i<n;i++)
+   for(i=0;i<N;i++)
    {
     scanf("%d",&n);
     x[n]++;
    }
-   for(i=0;i<10001;i++)
-   {
-      if(x[i]<max)
-      {
-         max = x[i];
-      }
 
-   }
-   for(i=0;i<10001;i++)
-   {
-      if(x[i]==max)
-         {
-            printf("%d",i);
-         }
-   }
 return 0;
 }
