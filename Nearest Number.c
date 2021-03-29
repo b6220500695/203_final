0eed07fb17a0b65d1c6e40a0799c56daaccb9be1
mmint.m <pheeraya.p@ku.th>
2021-03-29 19:56:22 +0700

    1 สุดท้าย

--- Nearest Number.c
+++ Nearest Number.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
 int main()
 {
-   int k,c,n,i;
+   int k,c,n;
    scanf("%d %d",&n,&c);
    if((n>0)&&(n<32750))
    {
       {
       n=n/10;
       k=n;
-      printf("%d ",n%10);
+      printf("%d",k);
       }
-      k=n*10;
-     if(k>n)
-      {
-      k=k/100;
-      printf(" %d",k+1);
-      }
-      printf(" %d",c);
+
+      printf("%d",c);
       }
    return 0;
 }
